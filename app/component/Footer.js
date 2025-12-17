ff4911edb32c0e1f42f206f0d8008f2061bd5a52
Muhammad Suleman <kingsuleman31@gmail.com>
2025-12-17 14:26:18 +0500

    contact form and font

--- app/component/Footer.js
+++ app/component/Footer.js
@@ -84,7 +84,7 @@ function Footer() {
               />
             </div>
 
-            <div className={`${styles.sectionlinks} ${isOpen("about") ? styles.open : ""}`}>
+            <div className={`${styles.sectionlinks} ${isOpen("about") ? styles.open : styles.close}`}>
               <a href="/about">About</a>
               <a href="/cookie-policy">Cookie Policy</a>
               <a href="/privacy-policy">Privacy Policy</a>
