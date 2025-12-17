6c4562190fbbf19b4906c503d87980b70b4768d2
Muhammad Suleman <kingsuleman31@gmail.com>
2025-12-17 22:47:27 +0500

    contact form and font

--- app/services/layout.js
+++ app/services/layout.js
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation";
 import { useRef, useState } from "react";
 import styles from "./layout.module.css";
 
+
 const services = [
   { name: "Assurance", path: "/services/assurance" },
   {
